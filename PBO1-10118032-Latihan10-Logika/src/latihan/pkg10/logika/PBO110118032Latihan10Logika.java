7ef950e8d2d602be78f4721fcdc42a13b7e393b2
Guntur Maulana Ibrahim <55784684+gunturmaulanaii@users.noreply.github.com>
2019-09-25 16:24:25 +0700

    Add files via upload

---
+++ PBO1-10118032-Latihan10-Logika/src/latihan/pkg10/logika/PBO110118032Latihan10Logika.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan.pkg10.logika;
+
+/**
+ *
+ * @author
+ * NAMA  : Guntur Maulana Ibrahim
+ * Kelas : PBO1
+ * NIM   : 10118032
+ * Deskripsi Program : Program ini berisi untuk menampilkan tipe data boolean yang bernilai true atau false
+ */
+public class PBO110118032Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
